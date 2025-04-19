ef242e2d1c91d7e97e38d199f8a384a4345dcaff
Seongjun Im <80080990+choral7451@users.noreply.github.com>
2025-04-19 23:11:53 +0900

    perf: ScanBibleVersesRequest validation 추가 필드에 Null 유효성검사를 추가했습니다

--- src/main/java/goj/holy365/api/bible/interfaces/controller/dto/in/ScanBibleVersesRequest.java
+++ src/main/java/goj/holy365/api/bible/interfaces/controller/dto/in/ScanBibleVersesRequest.java
@@ -2,15 +2,18 @@ package goj.holy365.api.bible.interfaces.controller.dto.in;
 
 import goj.holy365.api.bible.domain.service.dto.in.ScanVersesServiceDto;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ScanBibleVersesRequest {
+	@NotNull(message = "versionId 값을 입력해주세요.")
 	@Schema(description = "성경 버전 ID", example = "1")
 	Long versionId;
 
+	@NotNull(message = "bookId 값을 입력해주세요.")
 	@Schema(description = "성경 책 ID", example = "1")
 	Long bookId;
 
